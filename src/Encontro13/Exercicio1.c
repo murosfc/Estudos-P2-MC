3b1d33f97e680e7b426089ba2b2489493a53403b
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-09-11 22:58:15 -0300

    modificado algoritimo cholesky

--- src/Encontro13/Exercicio1.c
+++ src/Encontro13/Exercicio1.c
@@ -3,65 +3,85 @@
 #include <stdio.h>
 #include <math.h>
 
-void calcularTI(float L[T][T], float x[T], float y[T]){
+void calcularTI(float L[T][T], float x[T], float y[T])
+{
     float soma;
-    for (int i = 0; i<T; i++){
+    for (int i = 0; i < T; i++)
+    {
         soma = 0;
-        for (int j = 0; j<i; j++){
+        for (int j = 0; j < i; j++)
+        {
             soma += L[i][j] * x[j];
         }
         x[i] = (y[i] - soma) / L[i][i];
     }
 }
 
-void calcularTS(float L[T][T], float x[T], float y[T]){
+void calcularTS(float L[T][T], float x[T], float y[T])
+{
     float soma;
-    for(int i = T -1 ; i >=0 ; i--){
+    for (int i = T - 1; i >= 0; i--)
+    {
         soma = 0;
-        for(int j = i+1 ; j < T ; j++){
+        for (int j = i + 1; j < T; j++)
+        {
             soma += L[i][j] * x[j];
         }
         x[i] = (y[i] - soma) / L[i][i];
     }
-
 }
 
-void imprimirVetor(float x[T]){
-    for(int i = 0 ; i < T ; i++){
+void imprimirVetor(float x[T])
+{
+    for (int i = 0; i < T; i++)
+    {
         printf("%.0f ", x[i]);
     }
     printf("\n");
 }
 
-void decomportCholesky(int A[T][T], float R[T][T], float Rt[T][T]){
-    float soma;
-    for (int i=0; i<T; i++){
-        soma = 0;
-        for (int j=0; j<T; j++){
-            if (i==j){
-                for (int k=0; k<i; k++){
-                    soma+= pow(R[k][i], 2);
-                }
-                R[i][i] = sqrt(A[i][i] - soma)
-            }else{
-                for (int k=0; k<i; k++){
-                    soma+=R[k][i]*R[k][j]
-                }
-                R[i][j] = (A[i][j] - soma)/R[i][i]
+void calcularR(int A[T][T], float R[T][T])
+{
+    for (int i = 0; i < T; i++)
+    {
+        float sum = 0.0;
+        for (int k = 0; k < T; k++)
+        {
+            sum += pow(R[k][i], 2);
+        }
+        R[i][i] = sqrt(A[i][i] - sum);
+        for (int j = i + 1; j < T; j++)
+        {
+            sum = 0.0;
+            for (int k = 0; k < i; k++)
+            {
+                sum += R[k][i] * R[k][j];
             }
-            Rt[i][j] = R[j][i];
+            R[i][j] = 1 / R[i][i] * (A[i][j] - sum);
         }
     }
 }
 
-int main(){
-    int A[T][T] = {{1,2,4}, {2,8,10}, {4,10,26}};
+void calcularRT(float R[T][T], float Rt[T][T])
+{
+    for (int i = 0; i < T; i++)
+    {
+        for (int j = i; j < T; j++)
+            Rt[j][i] = R[i][j];
+    }
+}
+
+int main()
+{
+    int A[T][T] = {{1, 2, 4}, {2, 8, 10}, {4, 10, 26}};
     float R[T][T], Rt[T][T];
-    int b[T] = {1,-4,10};
-    float x[T],y[T];
-    decomportCholesky(A, R, Rt);
+    float b[T] = {1, -4, 10};
+    float x[T], y[T];
+    calcularR(A, R);
+    calcularRT(R, Rt);
+    // decomportCholesky(A, R, Rt);
     calcularTI(Rt, y, b);
-    calcularTR(R, x, y)
+    calcularTS(R, x, y);
     imprimirVetor(x);
     return 0;
 }
